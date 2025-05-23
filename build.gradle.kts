d69502abdc0ed88135790c83334acc5e170f67e7
Alexis <wamsteralexis@gmail.com>
2025-05-23 16:28:02 +0200

    Initialisation du projet (gradle + antlr)

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("java")
+    id("antlr")
+}
+
+group = "n7.mcdalang"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+sourceSets {
+    named("main") {
+        java {
+            srcDir(layout.buildDirectory.dir("/generated-src/antlr/main"))
+        }
+    }
+}
+
+dependencies {
+    antlr("org.antlr:antlr4:4.13.2") // ajouter par GPT
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.antlr:antlr4:4.13.2")
+    implementation("org.antlr:antlr4-runtime:4.13.2")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.generateGrammarSource {
+    arguments.addAll(listOf("-visitor", "-listener"))
+    outputDirectory = file(layout.buildDirectory.dir("/generated-src/antlr/main").get().asFile)
+}
+
+
+
